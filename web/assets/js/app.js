3861778b002f2f321b62949a0f5f9a732bfe5368
Damian Zabawa <damian.zabawa@gmail.com>
2018-08-09 09:52:07 +0200

    updates for Bootstrap4 (#138)
    updates for Bootstrap4

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,4 +1,49 @@
-$(document).ready(function(){
+jQuery(document).ready(function(){
+    var $ = jQuery;
+    // credits to http://codyhouse.co/gem/css-jquery-image-comparison-slider/
+    var drags = function (dragElement, resizeElement, container) {
+        dragElement.on('mousedown vmousedown', function (e) {
+            dragElement.addClass('draggable');
+            resizeElement.addClass('resizable');
+            var dragWidth = dragElement.outerWidth(),
+                xPosition = dragElement.offset().left + dragWidth - e.pageX,
+                containerOffset = container.offset().left,
+                containerWidth = container.outerWidth(),
+                minLeft = containerOffset + 10,
+                maxLeft = containerOffset + containerWidth - dragWidth - 10;
+            dragElement.parents().on('mousemove vmousemove', function (e) {
+                var leftValue = e.pageX + xPosition - dragWidth;
+                if(leftValue < minLeft ) {
+                    leftValue = minLeft;
+                } else if ( leftValue > maxLeft) {
+                    leftValue = maxLeft;
+                }
+                var widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
+                $('.draggable').css('left', widthValue).on('mouseup vmouseup', function () {
+                    $(this).removeClass('draggable');
+                    resizeElement.removeClass('resizable');
+                });
+                $('.resizable').css('width', widthValue);
+            }).on('mouseup vmouseup', function (e){
+                dragElement.removeClass('draggable');
+                resizeElement.removeClass('resizable');
+            });
+            e.preventDefault();
+        }).on('mouseup vmouseup', function (e) {
+            dragElement.removeClass('draggable');
+            resizeElement.removeClass('resizable');
+        });
+    };
+
+    $('.cd-resize-img img').width($('.cd-image-container img').css('width'));
+
+    drags($('.cd-handle'), $('.cd-resize-img'), $('.cd-image-container'));
+    $('.cd-image-container').addClass('is-visible');
+
+    $(window).on('resize', function () {
+        $('.cd-resize-img img').width($('.cd-image-container img').css('width'));
+    });
+
     var locationHash = document.location.hash;
 
     if (locationHash) {
